2515d3e32ac2d4211b979851dcb40b640264897e
Joel Fox Apelgren <joel.apelgren@makingwaves.com>
2022-03-16 20:27:24 +0100

    Add expected date.

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -2,18 +2,19 @@ const InfoBox = () => {
   return (
     <div className="InfoBox">
       <div className="Header">
-        <h2>Info</h2>
+        <h3>Info</h3>
+        <p>Förväntat datum: 2022-08-19</p>
       </div>
       <div className="ColFlex">
         <div>
-          <h3>Johanna</h3>
+          <h4>Johanna</h4>
           <p>
             4130 gram <br />
             55 cm
           </p>
         </div>
         <div>
-          <h3>Joel</h3>
+          <h4>Joel</h4>
           <p>
             2960 gram <br />
             49 cm
