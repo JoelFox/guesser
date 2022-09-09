f69a845584570140664ae3c6cb2a72589543f448
Joel Fox Apelgren <joel.apelgren@gmail.com>
2022-09-09 14:14:29 +0200

    List props

--- src/components/GuessList.js
+++ src/components/GuessList.js
@@ -10,7 +10,35 @@ const GuessList = () => {
     const data = getAllGuesses().then(x => setGuesses(x));
     console.log(data);
   }
-
+  /*
+  date
+  : 
+  "2022-08-25"
+  gender
+  : 
+  "flicka"
+  guesserName
+  : 
+  "Kusin Erik "
+  height
+  : 
+  51
+  nameSuggestions
+  : 
+  "Sofia "
+  time
+  : 
+  "02:32"
+  weight
+  : 
+  3652
+  welcomeMessage
+  : 
+  "Missa aldrig en kräftskiva på Vätö! "
+  
+  
+  
+  */
   return (
     <div>
       <h2>Gissningar</h2>
@@ -18,7 +46,14 @@ const GuessList = () => {
 
       {guesses && guesses.map(x =>
         <div>
-          Text
+          <div className='inline-block'>{x.guesserName}</div>
+          <div className='inline-block'>{x.date}</div>
+          <div className='inline-block'>{x.time}</div>
+          <div className='inline-block'>{x.gender}</div>
+          <div className='inline-block'>{x.height}</div>
+          <div className='inline-block'>{x.weight}</div>
+          <div className='inline-block'>{x.nameSuggestions}</div>
+          <div className='inline-block'>{x.welcomeMessage}</div>
         </div>
       )}
     </div>
